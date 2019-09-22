458d51047237a4ca6797fa84a1064f0822b22466
Skye Im <fluffyterran@gmail.com>
2019-09-21 19:21:25 -0700

    Feat ExpandedLights: Initial commit, no release.

---
+++ ExpandedLights/LightSystemPatch.cs
@@ -0,0 +1,77 @@
+﻿using Harmony;
+using System.Collections.Generic;
+using UnityEngine;
+using static SkyLib.Logger;
+using static SkyLib.OniUtils;
+
+namespace ExpandedLights
+{
+    public static class LightSystemPatch
+    {
+        public static string ModName = "LightSystemPatch";
+        public static bool didLightingHack = false;
+        public static Traverse ScanOctant;
+
+        public static class Mod_OnLoad
+        {
+            public static void OnLoad()
+            {
+                StartLogging(ModName);
+                ScanOctant = Traverse.Create(typeof(DiscreteShadowCaster))
+                    .Method("ScanOctant", new[] {
+                        typeof(Vector2I),
+                        typeof(int),
+                        typeof(int),
+                        typeof(DiscreteShadowCaster.Octant),
+                        typeof(double),
+                        typeof(double),
+                        typeof(List<int>)
+                    });
+            }
+        }
+
+        public static class ExtendedLightShapes
+        {
+            public const int ConeLeft = 991;
+            public const int ConeUp = 992;
+            public const int ConeRight = 993;
+            public const int Linear = 994;
+        }
+
+         [HarmonyPatch(typeof(DiscreteShadowCaster), "GetVisibleCells")]
+        public static class DiscreteShadowCaster_GetVisibleCells_Patch
+        {
+            public static bool Prefix(int cell, List<int> visiblePoints, int range, LightShape shape)
+            {
+                if ((int)shape < 2)
+                {
+                    return true;
+                }
+                // do the rest of the method
+                visiblePoints.Add(cell);
+                Vector2I xy = Grid.CellToXY(cell);
+
+                switch ((int)shape)
+                {
+                    case ExtendedLightShapes.ConeRight:
+                        ScanOctant.GetValue(xy, range, 1, DiscreteShadowCaster.Octant.E_NE, 1.0, 0.0, visiblePoints);
+                        ScanOctant.GetValue(xy, range, 1, DiscreteShadowCaster.Octant.E_SE, 1.0, 0.0, visiblePoints);
+                        break;
+                    case ExtendedLightShapes.ConeLeft:
+                        ScanOctant.GetValue(xy, range, 1, DiscreteShadowCaster.Octant.W_NW, 1.0, 0.0, visiblePoints);
+                        ScanOctant.GetValue(xy, range, 1, DiscreteShadowCaster.Octant.W_SW, 1.0, 0.0, visiblePoints);
+                        break;
+                    case ExtendedLightShapes.ConeUp:
+                        ScanOctant.GetValue(xy, range, 1, DiscreteShadowCaster.Octant.N_NE, 1.0, 0.0, visiblePoints);
+                        ScanOctant.GetValue(xy, range, 1, DiscreteShadowCaster.Octant.N_NW, 1.0, 0.0, visiblePoints);
+                        break;
+                    case ExtendedLightShapes.Linear:
+                        break;
+                    default:
+                        return true; // just give up :D
+                }
+                return false;
+            }
+        }
+    }
+}
---
+++ ExpandedLights/RotatableLight.cs
@@ -0,0 +1,42 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static ExpandedLights.LightSystemPatch;
+
+namespace ExpandedLights
+{
+    class RotatableLight: KMonoBehaviour
+    {
+        [MyCmpReq]
+        Rotatable rotatable;
+        [MyCmpReq]
+        Light2D light2D;
+        protected override void OnSpawn()
+        {
+            SkyLib.Logger.LogLine("DEBUG", rotatable.GetOrientation().ToString());
+            var pending_state = Traverse.Create(light2D).Field("pending_emitter_state").GetValue< LightGridManager.LightGridEmitter.State>();
+            pending_state.falloffRate = 0f;
+            light2D.emitter.Refresh(pending_state, true);
+            switch (rotatable.GetOrientation())
+            {
+                case Orientation.R90:
+                    light2D.shape = (LightShape)ExtendedLightShapes.ConeRight;
+                    break;
+                case Orientation.R180:
+                    light2D.shape = LightShape.Cone;
+                    break;
+                case Orientation.R270:
+                    light2D.shape = (LightShape)ExtendedLightShapes.ConeLeft;
+                    break;
+                case Orientation.Neutral:
+                    light2D.shape = (LightShape)ExtendedLightShapes.ConeUp;
+                    break;
+                default:
+                    light2D.shape = LightShape.Circle;
+                    break;
+            }
+        }
+    }
+}
