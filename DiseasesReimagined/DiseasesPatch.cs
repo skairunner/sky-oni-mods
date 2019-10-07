882b7aba062faaa943dc7e1fb04e1f143e92f50e
Peter Han <peterhaneve@outlook.com>
2019-10-06 23:09:26 -0700

    Feat Diseases: Crops when harvested transfer germs to the seed and the harvested items according to the ratios of their masses

--- DiseasesReimagined/DiseasesPatch.cs
+++ DiseasesReimagined/DiseasesPatch.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Reflection.Emit;
 using Harmony;
 using Klei.AI;
 using Klei.AI.DiseaseGrowthRules;
@@ -339,6 +340,92 @@ namespace DiseasesReimagined
                 {
                     throw new InvalidOperationException("Could not patch hand wash method!", e);
                 }
+            }
+        }
+
+        // Transfers germs from one object to another using their mass ratios
+        public static void TransferByMassRatio(GameObject parent, GameObject child)
+        {
+            if (parent == null)
+                throw new ArgumentNullException("parent");
+            if (child == null)
+                throw new ArgumentNullException("child");
+            PrimaryElement parentElement = parent.GetComponent<PrimaryElement>(),
+                childElement = child.GetComponent<PrimaryElement>();
+            float seedMass;
+            int germs, subGerms;
+            // Distribute the germs by mass ratio if there are any
+            if (parentElement != null && childElement != null && (seedMass = childElement.
+                Mass) > 0.0f && (germs = parentElement.DiseaseCount) > 0)
+            {
+                byte disease = parentElement.DiseaseIdx;
+                subGerms = Mathf.RoundToInt(germs * seedMass / (seedMass +
+                    parentElement.Mass));
+                // Seed germs
+                childElement.AddDisease(disease, subGerms, "TransferToChild");
+                // Plant germs
+                parentElement.AddDisease(disease, -subGerms, "TransferFromParent");
+            }
+        }
+
+        // Transfer germs from plant to fruit
+        [HarmonyPatch(typeof(Crop), "SpawnFruit")]
+        public static class Crop_SpawnFruit_Patch
+        {
+            // Transfers germs from crop to child (fruit)
+            internal static void AddGerms(Crop parent, GameObject child)
+            {
+                TransferByMassRatio(parent.gameObject, child);
+            }
+
+            public static IEnumerable<CodeInstruction> Transpiler(
+                IEnumerable<CodeInstruction> method)
+            {
+                MethodBase setTemp = null, germify = null;
+                // No easy way to get the game object without a replacement or transpiler
+                try
+                {
+                    var tempProp = typeof(PrimaryElement).GetProperty("Temperature",
+                        BindingFlags.Public | BindingFlags.Instance);
+                    // set_Temperature
+                    if (tempProp != null)
+                        setTemp = tempProp.GetSetMethod(true);
+                    germify = typeof(Crop_SpawnFruit_Patch).GetMethod(nameof(AddGerms),
+                        BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static);
+                }
+                catch (AmbiguousMatchException e)
+                {
+                    // This is not good
+                    PUtil.LogException(e);
+                }
+                foreach (var instr in method)
+                    if (instr.opcode == OpCodes.Callvirt && instr.operand == setTemp &&
+                        germify != null)
+                    {
+                        yield return instr;
+                        // ldarg.0 loads "this"
+                        yield return new CodeInstruction(OpCodes.Ldarg_0);
+                        // ldloc.1 loads the returned GameObject
+                        yield return new CodeInstruction(OpCodes.Ldloc_1);
+                        // Callvirt AddGerms
+                        yield return new CodeInstruction(OpCodes.Call, germify);
+                    }
+                    else
+                        // Rest of method
+                        yield return instr;
+            }
+        }
+
+        // Transfer germs from plant to seed
+        [HarmonyPatch(typeof(SeedProducer), "ProduceSeed")]
+        public static class SeedProducer_ProduceSeed_Patch
+        {
+            public static void Postfix(ref SeedProducer __instance, ref GameObject __result)
+            {
+                var seed = __result;
+                var obj = __instance.gameObject;
+                if (seed != null && obj != null)
+                    TransferByMassRatio(obj, seed);
             }
         }
 
