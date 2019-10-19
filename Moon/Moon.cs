6076f8c221f86e3eeafee3a46b1b7b31a61d2f25
Skye Im <fluffyterran@gmail.com>
2019-10-19 16:53:41 -0600

    Initial Moon

---
+++ Moon/Moon.cs
@@ -0,0 +1,26 @@
+﻿using Harmony;
+using PeterHan.PLib;
+using ProcGen;
+using static SkyLib.Logger;
+
+namespace Moon
+{
+    public static class Moon
+    {
+        public static void OnLoad()
+        {
+            StartLogging();
+        }
+        
+        // Load world strings
+        [HarmonyPatch(typeof(Db), "Initialize")]
+        public static class Db_Initialize_Patch
+        {
+            static void Postfix()
+            {
+                Strings.Add("STRINGS.WORLDS.MOON.NAME", "The Moon?");
+                Strings.Add("STRINGS.WORLDS.MOON.DESCRIPTION", "SPAAAAACE");
+            }
+        }
+    }
+}
