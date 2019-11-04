aab08d0069955366b3ea00e2e4a56b6d5d3bcbfe
Skye Im <fluffyterran@gmail.com>
2019-11-04 10:42:36 -0700

    Fix Restart annotation

--- CarbonRevolution/CarbonOptions.cs
+++ CarbonRevolution/CarbonOptions.cs
@@ -7,32 +7,26 @@ using SkyLib;
 
 namespace CarbonRevolution
 {
+    [RestartRequired]
     public class CarbonOption: SingletonOption<CarbonOption>
     {
-        
-        [RestartRequired]
         [Option("Coal Gen CO2", "How much CO2 in kg/s the Coal Gen emits.")][JsonProperty]
         public float CO2_coalgen { get; set; } = .25f;
 
-        [RestartRequired]
         [Option("Petrol Gen CO2", "How much CO2 in kg/s the Petrol Gen emits.")][JsonProperty]
         public float CO2_petrolgen { get; set; } = .5f;
         
-        [RestartRequired]
         [Option("Lumber Gen CO2", "How much CO2 in kg/s the Lumber Gen emits.")][JsonProperty]
         public float CO2_lumbergen { get; set; } = .17f;
 
-        [RestartRequired]
         [Limit(0, 1)]
         [Option("Nat Gas CO2", "How much CO2 in kg/s the Nat Gas Gen emits.")]
         [JsonProperty]
         public float CO2_natgasgen { get; set; } = .14f;
 
-        [RestartRequired]
         [Option("Gas Range CO2", "How much CO2 in kg/s the Gas Range emits.")][JsonProperty]
         public float CO2_gasrange { get; set; } = .1f;
         
-        [RestartRequired]
         [Option("Ethanol Distiller CO2", "How much CO2 in kg/s the Ethanol Distiller emits.")][JsonProperty] 
         public float CO2_ethanoldistiller { get; set; } = .16667f;
     }
--- RockCrusherConfiguration/RockCrusherOption.cs
+++ RockCrusherConfiguration/RockCrusherOption.cs
@@ -4,22 +4,20 @@ using PeterHan.PLib.Options;
 using SkyLib;
 
 namespace RockCrusherConfiguration
-{
+{        
+    [RestartRequired]
     public class RockCrusherOption: SingletonOption<RockCrusherOption>
     {
-        [RestartRequired]
         [Option("Lime fraction", "The % of fossil that is converted to Lime at a Rock Crusher.")]
         [Limit(0, 100)]
         [JsonProperty]
         public int LimeFraction { get; set; } = 5;
 
-        [RestartRequired]
         [Option("Refinement fraction", "The % of ore that is converted to Refined Metal at a Rock Crusher.")]
         [Limit(0, 100)]
         [JsonProperty]
         public int RefineFraction { get; set; } = 50;
-
-        [RestartRequired]
+        
         [Option("Salt amount", "The amount of Table Salt in grams that is produced from Salt a Rock Crusher.")]
         [JsonProperty]
         public float SaltAmount { get; set; } = 5f;
